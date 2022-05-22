6334b0e42f38defb4232cebd20a6b67f12a17953
DurvalSilva <silvadurvalservice@gmail.com>
2022-05-22 19:51:00 -0300

    confirmar em 'develop'

--- api/registerService.js
+++ api/registerService.js
@@ -1,63 +1,59 @@
 const _ = require('lodash')
-const Register = require('./register')
-const fullNameRegex = /^[/^[A-ZÀ-Ÿ][A-zÀ-ÿ']+\s([A-zÀ-ÿ']\s?)*[A-ZÀ-Ÿ][A-zÀ-ÿ']+$/
+const Register = require('./register');
 
 Register.methods(['get', 'post', 'put', 'delete'])
 Register.updateOptions({ new: true, runValidators: true })
 
-Register.after('post', sendErrosOrNext).after('put', sendErrosOrNext)
+Register.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext)
 Register.before('post', register).before('put', register)
 
-function sendErrosOrNext(req, res, next) {
-    const bundle = req.locals.bundle
-
-    if (bundle.erros) {
-        var erros = parseErros(bundles.erros)
-        res.status(500).json({ erros })
-    } else {
-        next()
-    }
+function sendErrorsOrNext(req, res, next) {
+  const bundle = res.locals.bundle
+  
+  if (bundle.errors) {
+    var errors = parseErrors(bundle.errors)
+    res.status(500).json({ errors })
+  } else {
+     next()    
+  }
 }
 
-function parseErros(nodeRestfulErros) {
-    const errors = []
-    _.forIn(nodeRestfulErros, error => error.push(error.message))
-    return errors
+function parseErrors(nodeRestfulErrors) {
+  const errors = []
+  _.forIn(nodeRestfulErrors, error => errors.push(error.message))
+  return errors
 }
 
-const sendErrosFromDB = (res, dbErrors) => {
-    const errors = []
-    _.forIn(dbErros.errors, error => errors.push(error.message))
-    return res.status(400).json({ erros })
+const sendErrorsFromDB = (res, dbErrors) => {
+  const errors = []
+  _.forIn(dbErrors.errors, error => errors.push(error.message))
+  return res.status(400).json({ errors })
 }
 
 function register(req, res, next) {
-    const fullName = req.body.fullName || ''
-    const mail = req.body.mail || ''
-    const phone = req.body.phone || ''
-    const address = req.body.address || ''
-    const number = req.body.number || ''
-    const complement = req.body.complement || ''
+  const fullName = req.body.fullName || ''
+  const mail = req.body.mail || ''
+  const phone = req.body.phone || ''
+  const address = req.body.address || ''
+  const number = req.body.number || ''
+  const complement = req.body.complement || '';
 
-    if(fullName == null || fullName == ""){
-        return res.status(400).send({ alert: ["O campo Nome Completo é obrigatório."]})
-    }
+  const newBody = new Register({
+      fullName,
+      mail,
+      phone,
+      address,
+      number,
+      complement
+  })
 
-    const newBody = new Register({
-        fullName,
-        mail,
-        phone,
-        address,
-        number,
-        complement,
-    })
-
-    newBody.save(err => {
-        if (err) {
-            return sendErrosFromDB(res, err)
-        } else {
-            res.status(201).json(newBody)
-        }
-    })
+  newBody.save(err => {
+      if (err) {
+          return sendErrorsFromDB(res, err)
+      } else {
+          res.status(201).json(newBody)
+      }
+  })
 }
+
 module.exports = Register
--- config/server.js
+++ config/server.js
@@ -6,8 +6,8 @@ const server = express()
 const queryParser = require('express-query-int')
 const allowCors = require('./cors')
 
-server.use(bodyParser.json({limit: '50mb'}));
-server.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 server.use(queryParser());
 server.use(allowCors);
 server.use(express.static('public'));
@@ -15,6 +15,5 @@ server.use(express.static('public'));
 server.listen(port, function(){
     console.log(`BACKEND is runner on port ${port}.`)
 })
-
 
 module.exports = server
