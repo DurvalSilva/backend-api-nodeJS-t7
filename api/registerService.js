4387f26d0509785084b5f393b3744f25a070da40
DurvalSilva <silvadurvalservice@gmail.com>
2022-05-23 00:42:10 -0300

    confirmar em 'develop'

--- api/registerService.js
+++ api/registerService.js
@@ -1,5 +1,9 @@
 const _ = require('lodash')
-const Register = require('./register');
+const Register = require('./register')
+const fullNameRegex = (/^[A-ZÀ-Ÿ][A-zÀ-ÿ']+\s([A-zÀ-ÿ']\s?)*[A-ZÀ-Ÿ][A-zÀ-ÿ']+$/)
+const mailRegex = /\S+@\S+\.\S+/;
+const phoneRegex = ('^((1[1-9])|([2-9][0-9]))((3[0-9]{3}[0-9]{4})|(9[0-9]{3}[0-9]{5}))$')
+
 
 Register.methods(['get', 'post', 'put', 'delete'])
 Register.updateOptions({ new: true, runValidators: true })
@@ -9,12 +13,12 @@ Register.before('post', register).before('put', register)
 
 function sendErrorsOrNext(req, res, next) {
   const bundle = res.locals.bundle
-  
+
   if (bundle.errors) {
     var errors = parseErrors(bundle.errors)
     res.status(500).json({ errors })
   } else {
-     next()    
+    next()
   }
 }
 
@@ -38,25 +42,35 @@ function register(req, res, next) {
   const number = req.body.number || ''
   const complement = req.body.complement || ''
 
-  if(fullName == null || fullName ==""){
-    return res.status(400).send({ alert: ["O campo Nome Completo é obrigatório"]})
-}
+  if (fullName == null || fullName == "") {
+    return res.status(400).send({ alert: ["O campo Nome Completo é obrigatório."] })
+  }
+  if (!fullName.match(fullNameRegex)) {
+    return res.status(400).send({ alert: ["Infomre o Nome e o Sobrenome."] })
+  }
 
+  if (mail == null || mail == "") {
+    return res.status(400).send({ alert: ["O campo Email é obrigatório."] })
+  }
+  if (!mail.match(mailRegex)) {
+    return res.status(400).send({ alert: ["O email informado é inválido. Informe um email non formato [nome@dominio.com ou nome@dominio.com.br]."] })
+  }
+  
   const newBody = new Register({
-      fullName,
-      mail,
-      phone,
-      address,
-      number,
-      complement
+    fullName,
+    mail,
+    phone,
+    address,
+    number,
+    complement
   })
 
   newBody.save(err => {
-      if (err) {
-          return sendErrorsFromDB(res, err)
-      } else {
-          res.status(201).json(newBody)
-      }
+    if (err) {
+      return sendErrorsFromDB(res, err)
+    } else {
+      res.status(201).json(newBody)
+    }
   })
 }
 
