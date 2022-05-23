d4e8d00a78459e593b1db9fb71f4f4d060b2b830
DurvalSilva <silvadurvalservice@gmail.com>
2022-05-22 22:18:35 -0300

    confirmar em 'develop'

--- config/routes.js
+++ config/routes.js
@@ -2,13 +2,13 @@ const express = require('express')
 
 module.exports = function (server) {
     const protectedApi = express.Router();
-    server.use('/api', protectedApi);
+    server.use("/api", protectedApi);
 
-    server.use('/status', (req, res) =>
-        res.send(`BACKEND is runner.`)
+    server.use("/status", (req, res) =>
+        res.send(`BACKEND is runner esta rodando 19hrs55.`)
     );
 
-    const register = require('../api/register/registerService');
+    const register = require('../api/registerService');
     register.register(protectedApi, '/register');
 
     server.use(express.static(require('path').join(__dirname, '../public')));
